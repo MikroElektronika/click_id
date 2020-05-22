948b6a125f6efe266b41efee6079fbfd0d6328a6
Demo User <debian@beaglebone.localdomain>
2020-05-22 12:56:36 +0000

    manifesto for mikrobus

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 for filename in manifests/*.mnfs; do
-    ./manifesto -o ${filename%.mnfs}.mnfb $filename
+    ./manifesto $filename
 done
 
-cp ./insclick /usr/bin/
-cp ./rmclick /usr/bin/
 cp ./manifesto /usr/bin/
 mkdir -p "/opt/manifesto/manifests"
-cp ./*.json /opt/manifesto/
 cp  ./manifests/*.mnfb /opt/manifesto/manifests
