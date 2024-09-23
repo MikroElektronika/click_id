9ebc57ba496d41d45574f40dee86d8e2b1eea601
Nenad Marinkovic <nenad.marinkovic@mikroe.com>
2024-09-23 14:50:26 +0200

    changed manifesto install script with makefile

--- manifesto/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for filename in manifests/*.mnfs; do
-    ./manifesto -i $filename
-done
-
-#cp ./manifesto /usr/bin/
-#mkdir -p "/opt/manifesto/manifests"
-#cp  ./manifests/*.mnfb /opt/manifesto/manifests
